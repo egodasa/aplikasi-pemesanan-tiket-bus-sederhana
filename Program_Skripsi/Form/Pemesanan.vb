e43dc0026e7533618180cac3d6b033f17b2cbb80
ego dasa <egodasa@hotmail.com>
2020-06-10 21:25:15 +0700

    Buat function koding form pemesanan

--- Program_Skripsi/Form/Pemesanan.vb
+++ Program_Skripsi/Form/Pemesanan.vb
@@ -1,5 +1,44 @@
 ﻿Public Class FPemesanan
-    Private Sub Pemesanan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub GenerateKodePemesanan()
+
+    End Sub
+    Private Sub TampilDetailLogin()
+
+    End Sub
+    Private Sub TampilDetailMobil()
+
+    End Sub
+    Private Sub TampilDetailTiket()
+
+    End Sub
+    Private Sub HitungPembayaran()
+
+    End Sub
+    Private Sub SimpanPemesanan()
+
+    End Sub
+    Private Sub CetakPemesanan()
+
+    End Sub
+    Private Sub HapusPemesanan()
+
+    End Sub
+    Private Sub ResetDataPembeli()
+
+    End Sub
+    Private Sub ResetDataMobil()
+
+    End Sub
+    Private Sub ResetDataTiket()
+
+    End Sub
+    Private Sub TampilDataPemesanan()
+
+    End Sub
+    Private Sub PencarianDataPemesanan(ByVal kata_kunci As String)
+
+    End Sub
+    Private Sub LoadForm(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
