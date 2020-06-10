84b7441e9668bab97505594e6d49e81bee3bec0d
ego dasa <egodasa@hotmail.com>
2020-06-10 21:14:15 +0700

    Penyesuaian kode form tiket

--- Program_Skripsi/Form/Tiket.vb
+++ Program_Skripsi/Form/Tiket.vb
@@ -1,26 +1,38 @@
 ﻿Public Class FTiket
-    Dim kd_domain As String
-    Dim nm_domain As String
+    Dim kode_tiket As String
+    Dim jurusan As String
+    Dim jumlah_bus As String
+    Dim kelas As String
+    Dim harga As String
+    Dim jumlah_tiket As String
     Private Sub AturData()
-        kd_domain = Tkode_tiket.Text
-        nm_domain = Tjurusan.Text
+        kode_tiket = Tkode_tiket.Text
+        jurusan = Tjurusan.Text
+        jumlah_bus = Tjumlah_bus.Text
+        kelas = Tkelas.Text
+        harga = Tharga.Text
+        jumlah_tiket = Tjumlah_tiket.Text
     End Sub
     Private Sub ResetForm(sender As Object, e As EventArgs) Handles Bbatal.Click
         Tkode_tiket.Clear()
         Tjurusan.Clear()
+        Tjumlah_tiket.Clear()
+        Tjumlah_bus.Clear()
+        Tkelas.Clear()
+        Tharga.Clear()
         Tkode_tiket.ReadOnly = False
     End Sub
 
     Private Sub TampilkanData(Optional ByVal kata_kunci As String = "")
-        Dim sql As String = "Select * FROM tbl_domain"
+        Dim sql As String = "Select * FROM tb_tiket"
 
         If kata_kunci <> "" Then
-            sql += " WHERE kd_domain LIKE '%" & kata_kunci & "%' OR nm_domain LIKE '%" & kata_kunci & "%' OR no_urut LIKE '%" & kata_kunci & "%'"
+            sql += " WHERE kode_tiket LIKE '%" & kata_kunci & "%' OR jurusan LIKE '%" & kata_kunci & "%' OR kelas LIKE '%" & kata_kunci & "%'"
         End If
         DGtiket.DataSource = Aplikasi.Db.JalankanDanAmbilData(sql)
     End Sub
-    Private Function CekKodeDomain()
-        Dim data As DataTable = Aplikasi.Db.JalankanDanAmbilData("SELECT * FROM tbl_domain WHERE kd_domain = '" & kd_domain & "'")
+    Private Function CekKodeTiket()
+        Dim data As DataTable = Aplikasi.Db.JalankanDanAmbilData("SELECT * FROM tb_tiket WHERE kode_tiket = '" & kode_tiket & "'")
         If data.Rows.Count > 0 Then
             Return False
         Else Return True
@@ -28,18 +40,22 @@
     End Function
     Private Sub AmbilData(sender As Object, e As DataGridViewCellEventArgs) Handles DGtiket.CellContentDoubleClick
         Dim data_terpilih As DataGridViewRow = DGtiket.CurrentRow
-        Tkode_tiket.Text = data_terpilih.Cells("kd_domain").Value
-        Tjurusan.Text = data_terpilih.Cells("nm_domain").Value
+        Tkode_tiket.Text = data_terpilih.Cells("kode_tiket").Value
+        Tjurusan.Text = data_terpilih.Cells("jurusan").Value
+        Tjumlah_bus.Text = data_terpilih.Cells("jumlah_bus").Value
+        Tkelas.Text = data_terpilih.Cells("kelas").Value
+        Tharga.Text = data_terpilih.Cells("harga").Value
+        Tjumlah_tiket.Text = data_terpilih.Cells("jumlah_tiket").Value
         Tkode_tiket.ReadOnly = True
     End Sub
     Private Sub ProsesTambahData(sender As Object, e As EventArgs) Handles Bsimpan.Click
         Me.AturData()
-        If CekKodeDomain() = False Then
-            MessageBox.Show("Kode domain sudah ada!Silahkan pilih kode domain lain.")
+        If CekKodeTiket() = False Then
+            MessageBox.Show("Kode tiket sudah ada! Silahkan pilih kode tiket lain.")
             Tkode_tiket.Clear()
             Return
         End If
-        Aplikasi.Db.JalankanSql("INSERT INTO tbl_domain VALUES ('" & kd_domain & "', '" & nm_domain & "')")
+        Aplikasi.Db.JalankanSql("INSERT INTO tb_tiket VALUES ('" & kode_tiket & "', '" & jurusan & "', '" & jumlah_bus & "', '" & kelas & "', '" & harga & "', '" & jumlah_tiket & "')")
         If Aplikasi.Db.ApakahError() Then
             MessageBox.Show("Error :" & Aplikasi.Db.AmbilPesanError())
         Else
@@ -50,7 +66,7 @@
     End Sub
     Private Sub ProsesEditData(sender As Object, e As EventArgs) Handles Bedit.Click
         Me.AturData()
-        Aplikasi.Db.JalankanSql("UPDATE tbl_domain SET nm_domain = '" & nm_domain & "' WHERE kd_domain = '" & kd_domain & "'")
+        Aplikasi.Db.JalankanSql("UPDATE tb_tiket SET jurusan = '" & jurusan & "', jumlah_bus = '" & jumlah_bus & "',kelas = '" & kelas & "', harga = '" & harga & "',jumlah_tiket = '" & jumlah_tiket & "' WHERE kode_tiket = '" & kode_tiket & "'")
         If Aplikasi.Db.ApakahError() Then
             MessageBox.Show("Error :" & Aplikasi.Db.AmbilPesanError())
         Else
@@ -60,8 +76,8 @@
         End If
     End Sub
     Private Sub ProsesHapusData(sender As Object, e As EventArgs) Handles Bhapus.Click
-        Dim kd_domain = DGtiket.CurrentRow.Cells("kd_domain").Value
-        Aplikasi.Db.JalankanSql("DELETE FROM tbl_domain WHERE kd_domain = '" & kd_domain & "'")
+        Dim kode_tiket = DGtiket.CurrentRow.Cells("kode_tiket").Value
+        Aplikasi.Db.JalankanSql("DELETE FROM tb_tiket WHERE kode_tiket = '" & kode_tiket & "'")
         If Aplikasi.Db.ApakahError() Then
             MessageBox.Show("Error :" & Aplikasi.Db.AmbilPesanError())
         Else
