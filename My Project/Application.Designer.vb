8b7208669bd9224deb79154a9e5d668b4d03087e
ego dasa <egodasa@hotmail.com>
2020-07-04 20:16:31 +0700

    Finishing program

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Program_Skripsi.Aplikasi
+            Me.MainForm = Global.Tugas_Akhir.Aplikasi
         End Sub
     End Class
 End Namespace
